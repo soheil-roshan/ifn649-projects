1d65680bdf7aaca8cffda2bf257efff0079be422
soheil-roshan <soheil_roshan92@yahoo.com>
2023-08-30 14:48:43 +1000

    final before week 6

--- Week2SoilSensor/Week2SoilSensor.ino
+++ Week2SoilSensor/Week2SoilSensor.ino
@@ -1,68 +1,17 @@
-#include "DHT.h"
-//#include <SoftwareSerial.h>
-
-#define DHTPIN 21      // Digital pin connected to the DHT sensor
-#define DHTTYPE DHT11   // DHT 11
-
-#define LEDPIN 11
-
-DHT dht(DHTPIN, DHTTYPE);
-
-// Teensy 5V <--> HC-05 Vcc
-// Teensy Ground <--> HC-05 GND
-#define rxPin 7 // Teensy pin 7 <--> HC-05 Tx
-#define txPin 8 // Teensy pin 8 <--> HC-05 Rx
-//SoftwareSerial BTSerial =  SoftwareSerial(rxPin, txPin);
-
+const int ledPin = 11;
+const int soilPin = 20;
+// the setup() method runs once, when the sketch starts
 void setup() {
-  // Setup serial for monitor
-  Serial.begin(9600); 
-
-  // Setup DHT Sensor
-  pinMode(DHTPIN, INPUT);
-  dht.begin();
-
-  // Setup Serial1 for BlueTooth
-  Serial1.begin(9600); // Default communication rate of the Bluetooth module
+// initialize the digital pin as an output.
+pinMode(ledPin, OUTPUT);
+Serial.begin(9800);
 }
-
 void loop() {
-  if(Serial1.available() > 0){ // Checks whether data is comming from the serial port
-    digitalWrite(LEDPIN, HIGH);
-    
-    float h = dht.readHumidity();
-    float t = dht.readTemperature();
-    float f = dht.readTemperature(true);
-
-    float hif = dht.computeHeatIndex(f, h);
-    float hic = dht.computeHeatIndex(t, h, false);  
-
-    Serial.print(F(" Humidity: "));
-    Serial.print(h);
-    Serial.print(F("%  Temperature: "));
-    Serial.print(t);
-    Serial.print(F("C "));
-    Serial.print(f);
-    Serial.print(F("F  Heat index: "));
-    Serial.print(hic);
-    Serial.print(F("C "));
-    Serial.print(hif);
-    Serial.println(F("F"));
-
-    Serial1.print(F(" Humidity: "));
-    Serial1.print(h);
-    Serial1.print(F("%  Temperature: "));
-    Serial1.print(t);
-    Serial1.print(F("C "));
-    Serial1.print(f);
-    Serial1.print(F("F  Heat index: "));
-    Serial1.print(hic);
-    Serial1.print(F("C "));
-    Serial1.print(hif);
-    Serial1.println(F("F"));
-    
-    delay(1000);
-    digitalWrite(LEDPIN, LOW);
-    delay(1000);
- }
+int val = analogRead(soilPin);
+Serial.print("Soil : ");
+Serial.println(val);
+digitalWrite(ledPin, HIGH); // set the LED on
+delay(1000); // wait for a second
+digitalWrite(ledPin, LOW); // set the LED off
+delay(100); // wait for a second
 }
